e44f5b7e5abbe0b50208d268601a663cf6356f3b
Abby Hilman <abbyhilman@gmail.ccom>
2021-03-16 09:51:33 +0700

    splash screen

--- src/SplashScreen/index.js
+++ src/SplashScreen/index.js
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {View, Text} from 'react-native';
+import SplashScreen from "react-native-lottie-splash-screen";
 
 const SplashScreen = () => {
+
+  useEffect(() => {
+    SplashScreen.hide(); // here
+  }, []);
   return (
     <View>
       <Text>Loading...</Text>
